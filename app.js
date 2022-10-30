810664181c721381e147eacf53a8a9aafeb3508c
Uladimir Hrydzin <hrydzin@gmail.com>
2022-10-30 14:01:49 +0400

    refactor: remove li tag selector according to rule 3.4

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var completedTasksHolder=document.querySelector(".completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.className ="li-item";
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
@@ -41,12 +42,12 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task imput-text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className = "btn btn-edit edit";
+    editButton.className = "btn edit";
 
-    deleteButton.className = "btn btn-delete delete";
+    deleteButton.className = "btn  delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -85,7 +86,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector('.imput-text');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
